abddcd5652d559dc74a9bb2005ac1bdad9f9c347
g-poiz <giovannymarchena@hotmail.com>
2020-11-15 15:48:54 -0400

    enter

---
+++ us_test.py
@@ -0,0 +1,5 @@
+# get user input
+num = ''
+while not num.isdigit():
+    num = input('Number of words to generate: ')
+num = int(num)
---
+++ user_gen.py
@@ -0,0 +1,8 @@
+
+
+
+# get user input
+num = ''
+while not num.isdigit():
+    num = input('Number of words to generate: ')
+num = int(num)
