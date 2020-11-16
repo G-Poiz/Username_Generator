ee3f2592be8a28bc34a64c5d2d4d7cae4c0ba9fd
g-poiz <giovannymarchena@hotmail.com>
2020-11-16 15:07:18 -0400

    Commit

--- user_gen.py
+++ user_gen.py
@@ -1,43 +1,40 @@
 import random
 import string
 
-def user_function():
 # get user input
-    num = int(input("Number of words to generate: "))
+num = int(input("Number of words to generate: "))
 # read word lists
-    with open('nouns.txt', 'r') as infile:
-        nouns = infile.read().strip(' \n').split('\n')
-    with open('adjec_tives.txt', 'r') as infile:
-        adjectives = infile.read().strip(' \n').split('\n')
+with open('nouns.txt', 'r') as infile:
+    nouns = infile.read().strip(' \n').split('\n')
+with open('adjec_tives.txt', 'r') as infile:
+    adjectives = infile.read().strip(' \n').split('\n')
 #read censor list
-    with open('black_list.txt', 'r') as inline:
-        censored = inline.read().strip(' \n').split('\n')
+with open('black_list.txt', 'r') as inline:
+    censored = inline.read().strip(' \n').split('\n')
 # generate usernames
-    for i in range(num):
+for i in range(num):
 
     # construct username
-        word1 = random.choice(adjectives)
-        word2 = random.choice(nouns)
+    word1 = random.choice(adjectives)
+    word2 = random.choice(nouns)
     
     #check if word2 is censored
-        if word2 in censored:
-         i -= 1
+    if word2 in censored:
+        i -= 1
         continue
     #else make and print the username
     #captilaize first letter
-        word1 = word1.title()
-        word2 = word2.title()
-        username = '{}{}{}'.format(word1, word2, random.randint(1, 99))
-        
+    word1 = word1.title()
+    word2 = word2.title()
+    username = '{}{}{}'.format(word1, word2, random.randint(1, 99))
 
     # success
-        print(username)
-    user_function()
+    print(username)
 
-#     # Write to file
-# f = open("user_name.txt", "a")
-# f.write(username +"\n")
-# f.close()
+    # Write to file
+f = open("user_name.txt", "a")
+f.write(username +"\n")
+f.close()
 
 # # open and read the file after the appending:
 # f = open("user_name.txt", "r")
