944091657891dc77b4ae7d67841742276b047ae6
g-poiz <giovannymarchena@hotmail.com>
2020-11-16 15:03:34 -0400

    Commit

--- us_test.py
+++ us_test.py
@@ -1,10 +1,23 @@
-def get_answer(prompt):
-    while True:
-        answer = input(prompt).strip().lower()
-        if answer in ('yes',  'y',  'accept' ):
-            return "You have Accepted!"
+
+
+# def get_answer(prompt):
+#     while True:
+#         answer = input(prompt).strip().lower()
+#         if answer in ('yes',  'y',  'accept' ):
+#             return "You have Accepted!"
             
-        elif  answer in ('no',  'n','decline'):
-           return "You have Declined!"
+#         elif  answer in ('no',  'n','decline'):
+#            return "You have Declined!"
             
-print(get_answer("This content includes sencitive content 'Accept' or 'Decline'?"))
+# print(get_answer("This content includes sencitive content 'Accept' or 'Decline'?"))
+
+# import msvcrt as m
+# # username_function()
+# print("What would you like to Generate today?" +
+#       input("Press 'ENTER' to continue..."))
+# # q1 = input("What would you like to Generate today?(Press 'ENTER' to continue)")
+
+
+# def wait():
+#     m.getch()
+
